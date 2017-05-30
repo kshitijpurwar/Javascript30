630eeecce0ff7748ee63046215822315d660f582
Kshitij Purwar <kshitij.purwar4@gmail.com>
2017-05-30 14:45:34 +0530

    Link fix, h2 for click

--- 0.TemplateBoilerplate/build/src/scripts.js
+++ 0.TemplateBoilerplate/build/src/scripts.js
@@ -2,11 +2,11 @@ window.onload = function() {
 
   console.log(' Just Edit the files in "/build/src" folder and you are good to go with Live Reload, Linting and a lot more !!');
 
-  const heading = document.querySelector('h1');
+  const heading = document.querySelector('h2');
 
   let name = "Kshitij Purwar";
 
-  let Iam = "Great & Awesome";
+  let Iam = "Decent";
 
   heading.addEventListener('click',() => {
     alert(`Hi, this Template was made by ${name} the ${Iam} developer, huh !!`)
